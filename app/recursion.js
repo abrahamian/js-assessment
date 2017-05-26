421f754921c3e1b2bcf4ce71c788095eb2c8b564
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2017-05-26 16:35:08 -0400

    recursion: tests passing.

--- app/recursion.js
+++ app/recursion.js
@@ -3,6 +3,23 @@ exports = typeof window === 'undefined' ? global : window;
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
 
+  	function getFiles(filesArray, node) {
+  		return node.files ? filesArray.concat(node.files.reduce(getFiles, [])) : filesArray.concat(node);
+  	}
+
+  	function locateDir(targetDir, dirName) {
+  		var i = 0;
+  		var subDirs = targetDir.files.filter(function(node){ return !!node.dir; });
+  		while(i < subDirs.length) {
+  			var node = subDirs[i];
+  			return node.dir == dirName ? node : locateDir(node, dirName);
+				i++;
+  		}
+  	}
+
+  	var targetDir = dirName ? locateDir(data, dirName) : data;
+
+		return targetDir.files.reduce(getFiles,[]);
   },
 
   permute: function(arr) {
