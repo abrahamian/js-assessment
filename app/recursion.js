beb32f98a09513682d092b9fa32e8aefda61bede
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2017-05-27 13:40:56 -0400

    Permutation: all tests passing

--- app/recursion.js
+++ app/recursion.js
@@ -24,14 +24,63 @@ exports.recursionAnswers = {
   },
 
   permute: function(arr) {
+  	return arr.reduce(function permute(permutations, item, index, arr) {
 
+			if(arr.length > 1) {
+				var arrayWithoutThisElement = arr.slice(0, index).concat(arr.slice(index + 1));
+				var permutationsOfArrayWithoutThisElement = arrayWithoutThisElement.reduce(permute, []);
+				var newPermutations = permutationsOfArrayWithoutThisElement.map(function(perm) { return [item].concat(perm); });
+				return permutations.concat(newPermutations);
+			}
+
+			return permutations.concat(item);
+
+		}, []);
   },
 
   fibonacci: function(n) {
-
+  	return (new Array(n).fill(null)).reduce(function(seq, val, i) {
+  		return seq.concat((seq[i-1] || 1) + (seq[i-2] || 0));
+  	}, [])[n-1];
   },
 
   validParentheses: function(n) {
+  	var permute = this.permute;
+
+  	var arr1 = new Array(n).fill("(")
+  	var arr2 = new Array(n).fill(")")
+
+  	var arr = arr1.concat(arr2);
+
+  	function removeOutermostParentheses(str){
+  		return str.slice(1).replace(")", "");
+  	}
+
+  	function hasValidParentheses(str){
+  		var firstOpeningIsBeforeFirstClosing = (str.indexOf("(") < str.indexOf(")"));
+  		var lastOpeningIsBeforeLastClosing = (str.lastIndexOf(")") > str.lastIndexOf("("));
+
+  		if ( firstOpeningIsBeforeFirstClosing && lastOpeningIsBeforeLastClosing ) {
+  			if(str.length == 2) {
+  				return true
+  			} else {
+					return hasValidParentheses(removeOutermostParentheses(str));
+  			}
+  		} else {
+  			return false;
+  		}
+
+  	}
+
+  	function uniqueArray(arr){
+  		return arr.reduce(function(acc, val, i, array) {
+  			return (acc.indexOf(val) > -1) ? acc : acc.concat([val]);
+  		}, []);
+  	}
+
+  	var arrayOfPermutationStrings = permute(arr).map(function(array){return array.join(""); })
+  	return uniqueArray(arrayOfPermutationStrings).filter(hasValidParentheses);
 
   }
+
 };
